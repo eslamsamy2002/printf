decb4af99068a418d8dfc4e3cc69b71166d9e45d
eslam <eslam@org.com>
2023-08-21 09:28:51 -0400

    printf

--- _printf.c
+++ _printf.c
@@ -1,57 +1,54 @@
 #include "main.h"
 /**
-* _printf - is a function that selects the correct function to print.
-* @format: format string containing the characters and the specifiers*
-* Return: number of the printed chars
-*/
+ * _printf - is a function that selects the correct function to print.
+ * @format: format string containing the characters and the specifiers*
+ * Return: number of the printed chars
+ */
 int _printf(const char *format, ...)
-{  int char_num = 0;
+{
+    int char_num = 0;
     va_list list;
+    va_start(list, format);
+    if (!format || (format[0] == '%' && !format[1]))
+        return (-1);
+    if (format[0] == '%' && format[1] == ' ' && !format[2])
+        return (-1);
 
-  if (format == NULL)
-      return (-1);
-  if (!format || (format[0] == '%' && !format[1]))
-      return (-1);
-  if (format[0] == '%' && format[1] == ' ' && !format[2])
-      return (-1);
+    while (*format)
+   {
+        if (*format != '%')
+        {
+            write(1, format, 1);
+            char_num++;
+        }
+        else
+        {
+            format++;
+            if (*format == '\0' || *format <-1)
+                {
+                    return -1;
+                }
 
-va_start(list, format);
-while (*format)
-{
-if (*format != '%')
-{
-write(1, format, 1);
-char_num++;
-}
-else
-{
-format++;
-if (*format == '\0')
-{
-break;
-}
-
-if (*format == '%')
-{
-write(1, format, 1);
-char_num++;
-}
-else if (*format == 'c')
-{
-char c = va_arg(list, int);
-
-write(1, &c, 1);
-char_num++;
-}
-else if (*format == 's')
-{
-   char *str = va_arg(list, char *);
-
-   write(1, str, strlen(str));
-   char_num += strlen(str);
-}
-}
-format++;
-} va_end(list);
-return (char_num);
+            if (*format == '%')
+            {
+                write(1, format, 1);
+                char_num++;
+            }
+            else if (*format == 'c')
+            {
+                char c = va_arg(list, int);
+                write(1, &c, 1);
+                char_num++;
+            }
+            else if (*format == 's')
+            {
+                char *str = va_arg(list, char *);
+                write(1, str, strlen(str));
+                char_num += strlen(str);
+            }
+        }
+        format++;
+    }
+    va_end(list);
+    return (char_num);
 }
