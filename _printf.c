b50afc69f52f011bebc76f9d6df306b7eed49bcf
eslam <eslam@org.com>
2023-08-21 08:02:47 -0400

    printf

--- _printf.c
+++ _printf.c
@@ -1,48 +1,57 @@
 #include "main.h"
-int _printf(const char *format, ...){
-    int char_num=0;
+/**
+* _printf - is a function that selects the correct function to print.
+* @format: format string containing the characters and the specifiers*
+* Return: number of the printed chars
+*/
+int _printf(const char *format, ...)
+{  int char_num = 0;
     va_list list;
-    if(format == NULL)
+
+  if (format == NULL)
+      return (-1);
+  if (!format || (format[0] == '%' && !format[1]))
+      return (-1);
+  if (format[0] == '%' && format[1] == ' ' && !format[2])
+      return (-1);
+
+va_start(list, format);
+while (*format)
+{
+    if (*format != '%')
     {
-        return (-1);
-    }
-    va_start(list,format);
-    while(*format)
-    {
-        if(*format != '%')
-        {
-            write(1,format,1);
+            write(1, format, 1);
             char_num++;
-        }
-        else {
+    }
+    else
+    {
             format++;
             if (*format == '\0')
+            {
                 break;
-
-
-            if  (*format == '%')
-            {
-                write(1,format,1);
-                char_num++;
             }
 
-            else if (*format == 'c')
-            {
-                char c = va_arg(list,int);
-                write(1,&c,1);
-                char_num++;
-            }
-            else if (*format == 's')
-            {
-                char *str = va_arg(list,char*);
-                write(1,str, strlen(str));
-                char_num+= strlen(str);
-            }
-         }
-        format++;
-    }
-    va_end(list);
-    return char_num;
+if (*format == '%')
+{
+write(1, format, 1);
+char_num++;
 }
+else if (*format == 'c')
+{
+char c = va_arg(list, int);
 
+write(1, &c, 1);
+char_num++;
+}
+else if (*format == 's')
+{
+   char *str = va_arg(list, char *);
 
+   write(1, str, strlen(str));
+   char_num += strlen(str);
+}
+}
+format++;
+} va_end(list);
+return (char_num);
+}
