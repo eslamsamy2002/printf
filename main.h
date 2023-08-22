acdea94ee673581231db73be4479943b4408f914
Aya11ali <alyzaghlol340@gmail.com>
2023-08-22 13:02:13 -0700

    task

--- main.h
+++ main.h
@@ -3,6 +3,7 @@
 #include <stdarg.h>
 #include <unistd.h>
 #include <string.h>
+#include <stdlib.h>
 
 int _printf(const char *format, ...);
 int take(char c, va_list args);
@@ -12,8 +13,11 @@ int strange_string(char c);
 int print_percent(int one);
 int print_int(va_list arg);
 int _putchar(char c);
-
-
+char *reverse_binary(char *s);
+char *copy_string(char *copyed_s, char *str, unsigned int n);
+unsigned int length_of_base(unsigned int num, int base);
+int convert_binary(va_list list);
+void printing_base(char *str);
 
 
 #endif
